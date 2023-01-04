468de9a71b1b8dc8ff8bc4559306f1048af2de2a
CA-MANOJ-VARMA <camanojvarma@gmail.com>
2023-01-04 04:46:06 +0000

    JAN 4 10:16

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
 import StateDetails from './components/StateDetails'
+import About from './components/About'
 
 const App = () => (
   <>
@@ -10,6 +11,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/state/:stateCode" component={StateDetails} />
+      <Route exact path="/About" component={About} />
     </Switch>
   </>
 )
---
+++ src/components/About/index.js
@@ -0,0 +1,75 @@
+import {Component} from 'react'
+import './index.css'
+
+class About extends Component {
+  state = {faqDetails: []}
+
+  componentDidMount() {
+    this.fetchFaqsDetail()
+  }
+
+  fetchFaqsDetail = async () => {
+    const faqDetailsPush = []
+    const baseUrl = 'https://apis.ccbp.in/covid19-faqs'
+    const options = {
+      method: 'GET',
+    }
+    const fetchDetails = await fetch(baseUrl, options)
+    const jsonData = await fetchDetails.json()
+    const faqsDataObject = jsonData.faq
+    const faqsData = Object.entries(faqsDataObject)
+    console.log(faqsDataObject)
+    faqsData.map(eachItem =>
+      faqDetailsPush.push([eachItem[1].question, eachItem[1].answer]),
+    )
+    this.setState({faqDetails: faqDetailsPush})
+    console.log('faqDetails push')
+    console.log(faqDetailsPush)
+  }
+
+  faqDetailsReturn = () => {
+    const {faqDetails} = this.state
+    console.log('faqDetails return')
+    console.log(faqDetails)
+    return (
+      <div>
+        {faqDetails.map(eachQuestion => {
+          const answer = eachQuestion[1]
+          console.log(eachQuestion[1])
+          if (
+            eachQuestion[1].includes(
+              '<a href="https://api.covid19india.org" target="_blank">api.covid19india.org</a>',
+            )
+          ) {
+            console.log('true')
+            eachQuestion[1].replaceAll(
+              '<a href="https://api.covid19india.org" target="_blank">api.covid19india.org</a>',
+              'None',
+            )
+          }
+
+          return (
+            <div className="css-question-answer-container">
+              <p className="css-question-para">{eachQuestion[0]}</p>
+              <p className="css-answer-para">{answer}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <>
+        <div className="css-about-container">
+          <h1>About</h1>
+          <h2>COVID-19 vaccines be ready for distribution</h2>
+          {this.faqDetailsReturn()}
+        </div>
+      </>
+    )
+  }
+}
+
+export default About
